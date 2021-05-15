e00da821eb12b65c6d709bb9c3f16a8b80ef6c61
JinseongHwang <eddy5360@naver.com>
2021-05-15 10:59:06 +0900

    feat: 초음파 센서를 활용한 미로 탈출

--- src/18-6차시-미로탈출.py
+++ src/18-6차시-미로탈출.py
@@ -12,22 +12,20 @@ import time
 motorB = Motor(Port.B)
 motorC = Motor(Port.C)
 
-# 컬러 센서 선언
-color_sensor1 = ColorSensor(Port.S1)
-color_sensor2 = ColorSensor(Port.S2)
-
-DEGREE = 45 * 4
+# 초음파 센서 선언
+Ultrasonic_Sensor = UltrasonicSensor(Port.S1)
 
 while True:
-    motorB.run(300)
-    motorC.run(300)
+    #  화면의 글자를 지우고나서, 현재 감지된 물체와의 거리를 표시한다
+    brick.display.clear()
+    brick.display.text(str(Ultrasonic_Sensor.distance()) + "mm")
 
-    if color_sensor1.reflection() < 15:
-        # 시계방향으로 포인트턴
-        motorB.run_angle(300, DEGREE / 2, Stop.BRAKE, False)
-        motorC.run_angle(300, -DEGREE / 2, Stop.BRAKE, True)
-
-    if color_sensor2.reflection() < 15:
-        # 반시계방향으로 포인트턴
-        motorB.run_angle(300, -DEGREE / 2, Stop.BRAKE, False)
-        motorC.run_angle(300, DEGREE / 2, Stop.BRAKE, True)
+    if Ultrasonic_Sensor.distance() >= 100:
+        # 물체와의 거리가 10cm보다 길다면 직진
+        motorB.run(300)
+        motorC.run(300)
+    else:
+        DEGREE = 90 * 2
+        # 시계 방향(우회전)으로 90도 회전 (포인트턴)
+        motorB.run_angle(300, DEGREE, Stop.BRAKE, False)
+        motorC.run_angle(300, -DEGREE, Stop.BRAKE, True)
